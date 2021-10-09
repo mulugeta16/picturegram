2de675ae123d474d7e3a7270903b2048baa95d5e
mulugeta16 <mulugeta2030@gmail.com>
2021-10-10 08:57:36 +0900

    comment

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  root "users#index"
+  get 'users/index'
+  get 'sesions/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  resources :users
+  resources :pictures
+  resources :favorites, only: [:create, :destroy, :index]
+
+  resources :pictures do
+    collection do
+    post :confirm
+    end
+  end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+end
